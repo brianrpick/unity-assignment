f02c9967580dbd4e23697414f1c17a51af23a74b
Brian Pick <brianr.pick@gmail.com>
2017-05-29 06:52:03 -0700

    Added create project to controller and project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,15 @@
 class ProjectsController < ApplicationController
+  def create
+    # create project to send to model method
+    project = {
+      id: params[:id], project_name: params[:projectName], creation_date: params[:creationDate], expiry_date: params[:expiryDate], enabled: params[:enabled], target_countries: params[:targetCountries], project_cost: params[:projectCost], project_url: params[:projectUrl], target_keys: params[:targetKeys]
+    }
+    # call model to create project
+    if Project.create_project(project)
+      # send success message
+      render json: { message: 'campaign is successfully created' }
+    else
+      render json: { message: Project.errors.as_json }
+    end
+  end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,13 @@
 class Project < ApplicationRecord
+  def self.create_project(project)
+    # unless declared make :enabled true
+    if project[:enabled].nil?
+      project[:enabled] = true
+    end
+    #open file, append with each project
+    open('projects.txt', 'a') do |line|
+      line << project.as_json
+      lineg << "\n"
+    end
+  end
 end
